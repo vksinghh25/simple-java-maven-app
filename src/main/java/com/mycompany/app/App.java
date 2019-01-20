e7bdcedf566c62046314a8cafd3c4f2747d80828
Vipan Singh <vksinghh@Vipans-Air.Dlink>
2019-01-20 22:43:59 +0530

    more changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Bonjour World!";
+    private final String message = "Konichiwa World!";
 
     public App() {}
 
--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Bonjour World!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Konichiwa World!" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Bonjour World!\"");
+            fail("\"message\" is not \"Konichiwa World!\"");
         }
     }
 
